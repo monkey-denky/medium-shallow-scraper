39b47453ee207c65d5206efb44bcc1a64beb2448
Dan Nguyen <nguyendan.denky@gmail.com>
2020-03-21 18:47:41 +0100

    cheerio rework

--- main.js
+++ main.js
@@ -4,11 +4,11 @@
 
 const Apify = require('apify');
 
-const { parseUrl } = require('./src/parser.js');
-const { crawl } = require('./src/crawler.js');
-const { request } = require('./src/request.js');
+const { parseUrl, parseClaps } = require('./src/parser.js');
+
 const moment = require('moment');
 
+/*
 Apify.main(async () => {
   // Get input of the actor.
   // If you'd like to have your input checked and generate a user interface
@@ -54,4 +54,134 @@ Apify.main(async () => {
   } catch (error) {
     console.error(error);
   }
+});
+*/
+
+Apify.main(async () => {
+  const stats = {
+    totalCrawledPages: 0,
+    totalFoundArticles: 0,
+  };
+  const input = await Apify.getInput();
+  console.log('Input:');
+  console.dir(input);
+  if (!input || !input.keyphrase) {
+    throw new Error(
+      'Invalid input, must be a JSON object with the "keyphrase" field!',
+    );
+  }
+
+  const url = parseUrl(input);
+  const requestQueue = await Apify.openRequestQueue();
+
+  await requestQueue.addRequest({
+    url,
+  });
+
+  const crawler = new Apify.CheerioCrawler({
+    requestQueue,
+    // The crawler downloads and processes the web pages in parallel, with a concurrency
+    // automatically managed based on the available system memory and CPU (see AutoscaledPool class).
+    // Here we define some hard limits for the concurrency.
+    minConcurrency: 5,
+    maxRequestRetries: 10,
+
+    // Increase the timeout for processing of each page.
+    handlePageTimeoutSecs: 60,
+
+    // This function is called if the page processing failed more than maxRequestRetries+1 times.
+    handleFailedRequestFunction: async ({ request }) => {
+      console.log(`Request ${request.url} failed twice.`);
+    },
+    handlePageFunction: async ({ request, response, body, contentType, $ }) => {
+      const finalHref = response.request.gotOptions.href;
+
+      const requestPath = request.url.slice(
+        request.url.lastIndexOf('/archive') + 8,
+        request.url.lenght,
+      );
+      const finalPath = finalHref.slice(
+        finalHref.lastIndexOf('/archive') + 8,
+        finalHref.lenght,
+      );
+
+      if (requestPath === finalPath) {
+        stats.totalCrawledPages++;
+        console.log('[OK]: ' + finalHref);
+        let data;
+        const dates = $('.timebucket a');
+        let currentIsDay = false;
+
+        dates.each((index, element) => {
+          if ($(element).attr('href') === request.url) {
+            currentIsDay = true;
+          }
+        });
+
+        //Is leaf?
+        if (!dates.text() || currentIsDay) {
+          data = $('.streamItem')
+            .get()
+            .map(listItem => {
+              const link = $(listItem).find(`a[data-action="open-post"]`);
+              const responses = $(listItem).find(`a[href*="#--responses"]`);
+              const author = $(listItem).find('a[data-user-id]');
+              const name = $(listItem).find('h3');
+              const claps = $(listItem).find(
+                `button[data-action="show-recommends"]`,
+              );
+              const date = $(listItem).find(`time`);
+
+              const obj = {};
+
+              if (link) obj.link = link.attr('href').split('?')[0];
+
+              if (author)
+                obj.author = {
+                  link: author.attr('href').split('?')[0],
+                  name: author.text().trim(),
+                };
+              if (name) obj.name = name.text().trim();
+              if (claps)
+                obj.claps = parseInt(parseClaps(claps.text().trim())) || 0;
+              if (responses)
+                obj.responses =
+                  parseInt(
+                    responses
+                      .text()
+                      .trim()
+                      .split(' ')[0],
+                  ) || 0;
+              if (date) {
+                obj.date = moment(date.attr('datetime'));
+              }
+              stats.totalFoundArticles++;
+              return obj;
+            });
+        } else {
+          await Apify.utils.enqueueLinks({
+            $: $,
+            requestQueue,
+            selector: '.timebucket a',
+          });
+        }
+        // Save the data to dataset.
+        if (data) {
+          await Apify.pushData({
+            url: finalHref,
+            data,
+          });
+        }
+      } else {
+        console.log('No artciles in: ' + finalHref);
+      }
+    },
+  });
+
+  const start = moment();
+  await crawler.run();
+  const end = moment();
+  stats.input = input;
+  stats.totalSeconds = end.diff(start, 'seconds');
+  await Apify.setValue('STATS', stats);
 });
